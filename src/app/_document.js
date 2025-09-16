7c34877aa6f65af08de907b009896b0a01d0e145
Jamil Ahmed <jamilcodes@gmail.com>
2025-09-16 19:20:22 +0500

    Create _document.js

---
+++ src/app/_document.js
@@ -0,0 +1,19 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="google-site-verification=lsdRd2YPdm8D3hXPtss6Q3QqlGivHS4dBc1kkIfJvX8"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
