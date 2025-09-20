c051e8b7ba41c6e3ec5384661b75efcb05f7f29c
Jamil Ahmed <jamilcodes@gmail.com>
2025-09-20 12:32:43 +0500

    Fixed Linkedin link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
             GitHub
           </a>
           <a
-            href="https://linkedin.com/in/jamilcodes"
+            href="https://www.linkedin.com/in/jamilcodes"
             target="_blank"
             className="text-base-content/80 hover:text-base-content transition-colors"
           >
@@ -135,7 +135,7 @@ const Navbar = () => {
               GitHub
             </a>
             <a
-              href="https://linkedin.com/in/jamilcodes"
+              href="https://www.linkedin.com/in/jamilcodes"
               target="_blank"
               className="text-base-content/80 hover:text-base-content"
             >
